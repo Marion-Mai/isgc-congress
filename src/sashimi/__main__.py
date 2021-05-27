3d0abe5c65d27b502cb1e35bfa5987ad43a3e526
Ale Abdo <abdo@member.fsf.org>
2021-05-27 18:12:40 +0200

    Add sashimi module for domain topic modeling

---
+++ src/sashimi/__main__.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python
+
+import abstractology
+import pandas as pd
+
+# not used, but avoids an import order bug
+import graph_tool
+
+graph_tool
+
+
+def reload():
+    """Live reload while developing"""
+    import importlib
+
+    importlib.reload(abstractology)
+
+
+def get_data():
+    df15 = pd.read_csv("abstracts_contents_2015.tsv", sep="\t")
+    df15 = df15.dropna(subset=["abstract_text"])
+    df17 = pd.read_csv("abstracts_contents_1719.tsv", sep="\t")
+    df17 = df17.dropna(subset=["abstract_text"])
+    df = df15.append(df17)
+    df = df.reset_index()
+
+    return df
+
+
+def _load_data(a, get_data=get_data):
+    a.data = get_data()
+    a.process_corpus(ngrams=1)  # no ngrams while I don't fix gensim on guix
+    corpus_name = "isgc_2015-2017.df"
+    if corpus_name not in a.loaded["data"]:
+        a.loaded["data"].append(corpus_name)
+        a.col_title = "abstract_title"
+        a.col_time = "year"
+        a.text_source = "abstract_text"
+
+
+def bootstrap():
+    a = abstractology.Graphology()
+    _load_data(a)
+    a.load_domain_topic_model()
+    a.set_graph(extend={"prop": "year"})
+    a.load_domain_chained_model()
+    a.register_config()
+
+    return a
+
+
+def load():
+    a = abstractology.Graphology("auto_abstractology/reports/config.json")
+    _load_data(a)
+
+    return a
+
+
+def plot(a):
+    a.plot_sashimi("ISGC 2015-2017")
+    a.plot_sashimi("ISGC 2015-2017", chained=True)
+
+
+def main():
+    try:
+        a = load()
+    except FileNotFoundError:
+        a = bootstrap()
+    plot(a)
+    a.data.columns
+    reload()
+
+
+if __name__ == "__main__":
+    main()
